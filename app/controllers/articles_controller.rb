e11cdb686b1acedfc9db4693fe4ae7bcada9b7fb
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-08-31 16:58:31 +0300

    add an update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,35 @@ class ArticlesController < ApplicationController
 
   def index
     articles = Article.all
+    render json: articles status: :ok
   end
 
   def show
-    article = Article.find(params[:id])
+    article = find_article
+    render json: article
+  end
+
+  def create
+    article = Article.create(article_params)
+    render json: article status: :created
+  end
+
+  def update
+    article = find_article
+    article.update(article_params)
+    render json: article status: :ok
+  end
+
+  
+
+  private
+
+  def find_article
+    Article.find(params[:id])
+  end
+
+  def article_params
+    params.permit(:firstname, :lastname, :email, :phonenumber, :message)
   end
 
 end
